927a1887acdaca3924451e7fd01f1b921dc9a79f
brycekunschick <150211483+brycekunschick@users.noreply.github.com>
2024-01-22 18:45:00 -0500

    1/9 - Project Created
    BooksSpring2024_sec02 Project Created

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BooksSpring2024_sec02.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
