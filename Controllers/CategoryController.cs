0ccadf2f13204c901bf1dae09dc3c51944d90e43
brycekunschick <150211483+brycekunschick@users.noreply.github.com>
2024-01-22 21:18:21 -0500

    1/16 - Category View, Bootsrap
    o	Added a category controller
    o	Added a razor view for Category
    o	Category view displays categories and has a button to create a new category
    o	Added a bootstrap theme

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using BooksSpring2024_sec02.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BooksSpring2024_sec02.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private BooksDBContext _dbContext;
+
+        public CategoryController(BooksDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()//list or fetch all objects (categories)
+        {
+            var listOfCategories = _dbContext.Categories.ToList();
+
+            return View(listOfCategories);
+        }
+    }
+}
