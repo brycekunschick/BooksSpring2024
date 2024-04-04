13f81244ac5a00b49f62aa2edd3f6da4c04a503e
brycekunschick <150211483+brycekunschick@users.noreply.github.com>
2024-04-04 13:40:55 -0400

    4/2 - Managing Orders

---
+++ Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,26 @@
+﻿using BooksSpring2024_sec02.Data;
+using BooksSpring2024_sec02.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BooksSpring2024_sec02.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    public class OrderController : Controller
+    {
+        private BooksDBContext _dbContext;
+
+        public OrderController(BooksDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Order> listOfOrders = _dbContext.Orders.Include(o => o.ApplicationUser);
+
+
+            return View(listOfOrders);
+        }
+    }
+}
--- Areas/Customer/Controllers/CartController.cs
+++ Areas/Customer/Controllers/CartController.cs
@@ -136,6 +136,84 @@ namespace BooksSpring2024_sec02.Areas.Customer.Controllers
         }
 
 
+        [HttpPost]
+        [ActionName("ReviewOrder")]
+        public IActionResult ReviewOrderPOST(ShoppingCartVM shoppingCartVM)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);//fetches the user ID
+
+            var cartItemsList = _dbContext.Carts.Where(c => c.UserId == userId).Include(c => c.Book);
+
+            shoppingCartVM.CartItems = cartItemsList;
+
+            foreach (var cartItem in shoppingCartVM.CartItems)
+            {
+                cartItem.SubTotal = cartItem.Book.Price * cartItem.Quantity; //subtotal for individual cart item
+
+                shoppingCartVM.Order.OrderTotal += cartItem.SubTotal;
+            }
+
+            shoppingCartVM.Order.ApplicationUser = _dbContext.ApplicationUsers.Find(userId);
+
+            shoppingCartVM.Order.CustomerName = shoppingCartVM.Order.ApplicationUser.Name;
+
+            shoppingCartVM.Order.StreetAddress = shoppingCartVM.Order.ApplicationUser.StreetAddress;
+
+            shoppingCartVM.Order.City = shoppingCartVM.Order.ApplicationUser.City;
+
+            shoppingCartVM.Order.State = shoppingCartVM.Order.ApplicationUser.State;
+
+            shoppingCartVM.Order.PostalCode = shoppingCartVM.Order.ApplicationUser.PostalCode;
+
+            shoppingCartVM.Order.Phone = shoppingCartVM.Order.Phone;
+
+            shoppingCartVM.Order.OrderDate = DateOnly.FromDateTime(DateTime.Now);
+
+            shoppingCartVM.Order.OrderStatus = "Pending";
+
+            shoppingCartVM.Order.PaymentStatus = "Pending";
+
+
+            _dbContext.Orders.Add(shoppingCartVM.Order); //creates a new Order and generates an OrderId which then can be used to add OrderDetails
+
+            _dbContext.SaveChanges();
+
+            foreach(var eachCartItem in shoppingCartVM.CartItems)
+            {
+                OrderDetail orderDetail = new()
+                {
+                    OrderId = shoppingCartVM.Order.OrderId,
+                    BookId = eachCartItem.BookId,
+                    Quantity = eachCartItem.Quantity,
+                    Price = eachCartItem.Book.Price
+                };
+
+                _dbContext.OrderDetails.Add(orderDetail);
+
+            }
+
+            _dbContext.SaveChanges();   
+
+            return RedirectToAction("OrderConfirmation", new { id = shoppingCartVM.Order.OrderId});
+
+        }
+        
+
+
+        public IActionResult OrderConfirmation(int id)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            List<Cart> listOfCartItems = _dbContext.Carts.ToList().Where(c => c.UserId == userId).ToList();
+
+            _dbContext.Carts.RemoveRange(listOfCartItems);
+            _dbContext.SaveChanges();
+
+
+            return View(id);
+        }
+
+
 
 
     }
